bff3656ac2fdba82393d0852daa98b884011bb7b
Simone Pitzianti <simone.pitzianti@sardex.net>
2018-05-16 12:26:13 +0200

    Add manage ingredients feature

--- recipy/src/pages/new-recipe/new-recipe.ts
+++ recipy/src/pages/new-recipe/new-recipe.ts
@@ -1,23 +1,99 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { ActionSheetController, IonicPage, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
   selector: 'page-new-recipe',
   templateUrl: 'new-recipe.html',
 })
+//NOTE: reactive approach
 export class NewRecipePage {
-  newRecipe = {
-    title: '',
-    description: '',
-    difficulty: 0
-  };
+  newRecipeForm: FormGroup;
+  difficultyOptions = ['Easy', 'Medium', 'Hard'];
 
-  logForm(form) {
-    console.log(form.value);
+  constructor(
+    private actionSheetController: ActionSheetController,
+    private alertController: AlertController
+  ) {
+    this.initializeForm();
   }
 
-  manageIngredient() {
-    console.log('manageIngredient');
+  submit() {
+    console.log(this.newRecipeForm);
+  }
+
+  manageIngredients() {
+    this.manageActionSheet();
+  }
+
+  private initializeForm() {
+    this.newRecipeForm = new FormGroup(
+      {
+        'title': new FormControl(null, Validators.required),
+        'description': new FormControl(null, Validators.required),
+        'difficulty': new FormControl('Medium', Validators.required)
+      }
+    );
+  }
+
+  private manageActionSheet() {
+    let actionSheet = this.actionSheetController.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Add Ingredient',
+          handler: () => {
+            console.log('Add Ingredient clicked');
+            this.addIngredientAlert();
+          }
+        },
+        {
+          text: 'Remove all Ingredients',
+          role: 'destructive',
+          handler: () => {
+            console.log('Remove all Ingredients clicked');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  private addIngredientAlert() {
+    let alert = this.alertController.create({
+      title: 'Add Ingredint',
+      inputs: [
+        {
+          name: 'ingredient',
+          placeholder: 'Salsiccia molta'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            //TODO: add validation
+            console.log(data);
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
